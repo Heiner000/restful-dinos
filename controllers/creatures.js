27d87aa339fa4d55869e01209b8e863b9e686f15
Heiner000 <garrett.heiner@gmail.com>
2023-03-30 21:10:09 -0700

    fixed imgs, broke new dino/critter form

--- controllers/creatures.js
+++ controllers/creatures.js
@@ -8,6 +8,11 @@ const readCreatures = () => {
     const creatureData = JSON.parse(creatures)
     return creatureData
 }
+
+
+
+
+
 readCreatures()
 
 // GET /prehistoric_creatures -- READ displays all prehistoric creatures
@@ -17,33 +22,44 @@ router.get('/creatures', (req, res) => {
 
     if (req.query.creatureFilter) {
         creatures = creatures.filter(creature => {
-            return creature.name.toLowerCase().includes(req.query.creatureFilter.toLowerCase())
+            return creature.type.toLowerCase().includes(req.query.creatureFilter.toLowerCase())
         })
     }
     res.render('creatures/index.ejs', {
         creatures
     })
 })
-// GET /prehistoric_creatures/:id -- READ displays a single creature @ :id
-router.get('/creatures/:id', (req, res) => {
+
+
+// GET /prehistoric_creatures/new -- READ form for adding new prehistoric creatures
+router.get('/prehistoric_creatures/new', (req, res) => {
+    res.render('creatures/new.ejs')
+})
+
+// POST /prehistoric_creatures -- CREATE a new creature for the db
+router.post('/prehistoric_creatures', (req, res) => {
+    console.log(req.body)
     const creatures = readCreatures()
+
+    creatures.push(req.body)
+    
+    fs.writeFileSync('./prehistoric_creatures.json', JSON.stringify(creatures))
+
+
+    res.redirect('/prehistoric_creatures')
+})
+
+// GET /prehistoric_creatures/:id -- READ displays a single creature @ :id
+router.get('/prehistoric_creatures/:id', (req, res) => {
+
+    const creatures = readCreatures()
+    
     const foundCreature = creatures[req.params.id]
+    
     res.render('creatures/details.ejs', {
         creature: foundCreature,
         id: req.params.id
     })
-})
-// GET /prehistoric_creatures/new -- READ form for adding new prehistoric creatures
-router.get('/creatures/new', (req, res) => {
-    res.render('creatures/new.ejs')
-})
-// POST /prehistoric_creatures -- CREATE a new creature for the db
-router.post('/creatures', (req, res) => {
-    console.log(req.body)
-    const creatures = readCreatures
-    creatures.push(req.body)
-    fs.writeFileSync('./creatures.json', JSON.stringify(creatures))
-    res.redirect('/creatures')
 })
 
 module.exports = router
